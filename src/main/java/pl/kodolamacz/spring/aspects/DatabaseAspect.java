c80dcbfd398bc319e80594a9e30505f7b08cef98
dynekpl <dynek82@gmail.com>
2017-11-04 10:43:48 +0100

    Aspects example

---
+++ src/main/java/pl/kodolamacz/spring/aspects/DatabaseAspect.java
@@ -0,0 +1,38 @@
+package pl.kodolamacz.spring.aspects;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Controller;
+
+import java.util.Arrays;
+
+@Controller
+@Aspect
+public class DatabaseAspect {
+
+  @Before("execution(* pl.kodolamacz.spring.dao.repository.AbstractDao.save(..))")
+  public void before(JoinPoint joinPoint) {
+    System.out.println("Before save");
+  }
+
+  @After("execution(* pl.kodolamacz.spring.dao.repository.AbstractDao.save(..))")
+  public void after(JoinPoint joinPoint) {
+    System.out.println("After save");
+    System.out.println(joinPoint.getSignature().getName());
+    System.out.println(Arrays.toString(joinPoint.getArgs()));
+  }
+
+  @Around("execution(* pl.kodolamacz.spring.dao.repository.AbstractDao.findById(..))")
+  public void around(ProceedingJoinPoint joinPoint)  throws Throwable {
+    System.out.println("Around start");
+
+    Object proceed = joinPoint.proceed();// wykonuje macierzystą metodę
+
+    System.out.println("Around stop");
+  }
+
+}
--- src/main/java/pl/kodolamacz/spring/dao/model/User.java
+++ src/main/java/pl/kodolamacz/spring/dao/model/User.java
@@ -25,4 +25,12 @@ public class User extends Entity {
     public void setPassword(String password) {
         this.password = password;
     }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
 }
