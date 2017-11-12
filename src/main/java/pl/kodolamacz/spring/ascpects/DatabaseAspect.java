1f60c81262841769189cd045fe068ee7135e258a
dynekpl <dynek82@gmail.com>
2017-11-12 15:24:23 +0100

    Java annotations

--- src/main/java/pl/kodolamacz/spring/ascpects/DatabaseAspect.java
+++ src/main/java/pl/kodolamacz/spring/ascpects/DatabaseAspect.java
@@ -10,13 +10,29 @@ import org.springframework.stereotype.Controller;
 
 import java.util.Arrays;
 
+@Controller
+@Aspect
 public class DatabaseAspect {
 
+  @Before("execution(* pl.kodolamacz.spring.dao.repository.AbstractDao.save(..))")
   public void before(JoinPoint joinPoint) {
     System.out.println("Before save");
   }
 
+  @After("execution(* pl.kodolamacz.spring.dao.repository.AbstractDao.save(..))")
   public void after(JoinPoint joinPoint) {
+    System.out.println("After save");
+    System.out.println(joinPoint.getSignature().getName());
+    System.out.println(Arrays.toString(joinPoint.getArgs()));
+  }
+
+  @Around("execution(* pl.kodolamacz.spring.dao.repository.AbstractDao.findById(..))")
+  public void around(ProceedingJoinPoint joinPoint)  throws Throwable {
+    System.out.println("Around start");
+
+    Object proceed = joinPoint.proceed();// wykonuje macierzystą metodę
+
+    System.out.println("Around stop");
   }
 }
 
--- src/main/java/pl/kodolamacz/spring/dao/model/User.java
+++ src/main/java/pl/kodolamacz/spring/dao/model/User.java
@@ -2,27 +2,35 @@ package pl.kodolamacz.spring.dao.model;
 
 public class User extends Entity {
 
-    private String email;
-    private String password;
+  private String email;
+  private String password;
 
-    public User(String email, String password) {
-        this.email = email;
-        this.password = password;
-    }
+  public User(String email, String password) {
+    this.email = email;
+    this.password = password;
+  }
 
-    public String getEmail() {
-        return email;
-    }
+  public String getEmail() {
+    return email;
+  }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
+  public void setEmail(String email) {
+    this.email = email;
+  }
 
-    public String getPassword() {
-        return password;
-    }
+  public String getPassword() {
+    return password;
+  }
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  @Override
+  public String toString() {
+    return "User{" +
+            "email='" + email + '\'' +
+            ", password='" + password + '\'' +
+            '}';
+  }
 }
