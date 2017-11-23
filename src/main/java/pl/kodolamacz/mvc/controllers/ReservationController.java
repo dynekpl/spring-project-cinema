56d52579b2a8f327396a3ce0d48165c7032fc7fd
dynekpl <dynek82@gmail.com>
2017-11-23 23:40:22 +0100

    GET with PathVariable example

--- src/main/java/pl/kodolamacz/mvc/controllers/ReservationController.java
+++ src/main/java/pl/kodolamacz/mvc/controllers/ReservationController.java
@@ -3,6 +3,7 @@ package pl.kodolamacz.mvc.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -25,10 +26,21 @@ public class ReservationController {
   }
 
   // przykład adresu: http://localhost:8080/kino/reservations/byUser?userId=1
-  @RequestMapping(value = "/byUser", method = RequestMethod.GET)
+  @RequestMapping(value = "/byUsers", method = RequestMethod.GET)
   public String getReservationsByUserId(Model model, @RequestParam("userId") Long userId) {
     // Jeśli paramName oraz nazwa zmiennej się pokrywają
     // to można pominąć deklarację nazwy zmiennej w @RequestParam
+    model.addAttribute("userReservations", reservationDao.findByUserId(userId));
+    model.addAttribute("user", userDao.findOne(userId));
+    return "reservations";
+  }
+
+  // przykład adresu: http://localhost:8080/kino/reservations/byUser2/2
+  @RequestMapping(value = "/byUser/{userId}", method = RequestMethod.GET)
+  public String getReservationsByUserId2(Model model, @PathVariable("userId") Long userId) { // parametrów może być więcej
+    // Jeśli paramName oraz nazwa zmiennej się pokrywają
+    // to można pominąć deklarację nazwy zmiennej w @Pathvariable
+    // wiecej parametrów np.: http://localhost:8080/kino/reservations/byUser2/2/Arek/1988
     model.addAttribute("userReservations", reservationDao.findByUserId(userId));
     model.addAttribute("user", userDao.findOne(userId));
     return "reservations";
